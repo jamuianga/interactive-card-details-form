4b34825e2d79565b1617a20bdeb944c4fda7f894
Jânio Muianga <janiodrey@gmail.com>
2022-09-08 00:56:57 +0200

    update card info name, cvc

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import card_logo from './assets/card-logo.svg';
 import Form from './components/Form';
 
 function App() {
+  const [cardholderName, setCardholderName] = useState('Jane Appleseed');
+  const [cardInfo, setCardInfo] = useState({
+    cardholderName: 'Jane Appleseed',
+    cvc: '000',
+  });
+
   return (
     <div className="wrapper">
       <div className="left-panel">
@@ -12,17 +18,17 @@ function App() {
           <div className="card-details">
             <div className="number">0000 0000 0000 0000 0000</div>
             <div className="name-date">
-              <div className="name">Jane Appleseed</div>
+              <div className="name">{cardInfo.cardholderName}</div>
               <div className="date">00/00</div>
             </div>
           </div>
         </div>
         <div className="card card-back">
-          <div className="cvc">000</div>
+          <div className="cvc">{cardInfo.cvc}</div>
         </div>
       </div>
       <div className="right-panel">
-        <Form/>
+        <Form setCardInfo={setCardInfo} />
       </div>
     </div>
   );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Form.scss';
 
-function Form() {
+function Form({ setCardInfo }) {
+  const cardholderNameOnChange = (e) => {
+    setCardInfo((prevCardInfo) => {
+      return {
+        ...prevCardInfo,
+        cardholderName: e.target.value,
+      };
+    });
+  };
+
+  const cvcOnChange = (e) => {
+    setCardInfo((prevCardInfo) => {
+      return {
+        ...prevCardInfo,
+        cvc: e.target.value,
+      };
+    });
+  };
+
   return (
     <form>
       <div className="form-group">
         <label>Cardholder name</label>
-        <input type="text" placeholder="e.g. Jane Appleseed" />
+        <input
+          type="text"
+          placeholder="e.g. Jane Appleseed"
+          onChange={cardholderNameOnChange}
+        />
         <small className="error-msg"></small>
       </div>
       <div className="form-group">
@@ -18,14 +40,20 @@ function Form() {
         <div className="form-group">
           <label htmlFor="">Exp. Date (mm/yy)</label>
           <div className="inline-input expire-date">
-            <input type="text" placeholder="MM" maxLength={2}/>
-            <input type="text" placeholder="YY" maxLength={2}/>
+            <input type="text" placeholder="MM" maxLength={2} />
+            <input type="text" placeholder="YY" maxLength={2} />
           </div>
           <small className="error-msg"></small>
         </div>
         <div className="form-group">
-          <label htmlFor="">cvc</label>
-          <input type="text" placeholder="e.g. 123" />
+          <label>cvc</label>
+          <input
+            type="text"
+            placeholder="e.g. 123"
+            className="cvc"
+            maxLength={3}
+            onChange={cvcOnChange}
+          />
           <small className="error-msg"></small>
         </div>
       </div>
