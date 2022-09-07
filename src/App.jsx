cdff4b75c2d90173fbff2bb81310a73b42db0fb4
Jânio Muianga <janiodrey@gmail.com>
2022-09-07 12:47:27 +0200

    design front card

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.scss';
+import card_logo from './assets/card-logo.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="wrapper">
+      <div className="left-panel">
+        <div className="card card-front">
+          <img src={card_logo} alt="Card logo" />
+          <div className="card-details">
+            <div className="number">0000 0000 0000 0000 0000</div>
+            <div className="name-date">
+              <div className="name">John Doe</div>
+              <div className="date">00/00</div>
+            </div>
+          </div>
+        </div>
+        <div className="card card-back">card back</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="right-panel">
+        <div className="card-form">form</div>
+        <div className="card-form">form submited</div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
